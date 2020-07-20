b30e96772c3dd65b6c74cb58344a307aa5c1728b
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-20 00:21:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)# code an argument here
   array.collect { |word| word.capitalize + "!" } # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array) # code an argument here
+  array.any? { |word| word.length > 4} # Your code here
 end
 
 def find_the_cheese# code an argument here
