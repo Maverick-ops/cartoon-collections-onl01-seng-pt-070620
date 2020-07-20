12624a10c23c386a8628aefc684f48311f084b8a
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-20 00:19:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,6 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index { |word, index|
+  puts "#{index+1} = #{word}"} # code an argument here
   # Your code here
 end
 
